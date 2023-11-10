5a7154adced47c07ef3e4ff4c7694e4fc09d57a6
Máté Marcalek <matemarcalek@gmail.com>
2023-11-10 10:54:23 +0100

    Add project files.

---
+++ FeleviEredmenyek231110/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FeleviEredmenyek231110
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Tanulo> tanulok = new();
+            List<string> tanorak = new();
+            using StreamReader sr = new(
+                path: @"..\..\..\src\Eredmenyek.txt",
+                encoding: UTF8Encoding.UTF8);
+            string sor = sr.ReadLine();
+            while (!sr.EndOfStream) if(sr.ReadLine() != null) tanulok.Add(new(sr.ReadLine()));
+            var v = sor.Split('\t');
+            for (int i = 2; i < v.Length; i++)
+            {
+                tanorak.Add(v[i]);
+            }
+
+            Console.WriteLine("1. feladat:");
+            Console.WriteLine($"\tAz osztályátlag:  {Osztalyatlag(tanulok)}");
+
+            Console.WriteLine("2. feladat:");
+            int index = tanorak.IndexOf("Programozás gyakorlat");
+            var bukottak = tanulok.Where(t => t.Jegyek[index] == 1);
+            foreach (var b in bukottak)
+            {
+                Console.WriteLine($"\tNév:  {b.Nev}\n\tTanulóikód:  {b.Kod}");
+            }
+
+            using StreamWriter sw = new(
+                path: @"..\..\..\src\Eredmenyek2.txt",
+                append: false,
+                encoding: UTF8Encoding.UTF8);
+            foreach (var t in tanulok)
+            {
+                sw.WriteLine($"{t.Nev}\t{t.Kod}");
+            }
+            Console.ReadLine();
+        }
+
+        static double Osztalyatlag(List<Tanulo> tanulok)
+        {
+            double atlag = 0;
+            foreach (var t in tanulok)
+            {
+                atlag += t.Atlag;
+            }
+            atlag /= tanulok.Count();
+            return atlag;
+        }
+    }
+}
---
+++ FeleviEredmenyek231110/Tanulo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeleviEredmenyek231110
+{
+    class Tanulo
+    {
+        public string Nev { get; set; }
+        public string Kod { get; set; }
+        public List<int> Jegyek { get; set; }
+        public double Atlag => Jegyek.Average();
+
+        public Tanulo(string sor)
+        {
+            var v = sor.Split('\t');
+            Nev = v[0];
+            Kod = v[1];
+            List<int> jegyek = new();
+            for (int i = 2; i < v.Length; i++)
+            {
+                jegyek.Add(int.Parse(v[i]));
+            }
+            Jegyek = jegyek;
+        }
+
+        
+    }
+}
